32fabd8af2ca223d89cd4e30ce04616c572e8659
IlyaBabich1994 <ktljva@gmail.com>
2023-04-11 12:34:16 +0200

    Init

---
+++ src/main/java/org/scope/models/Cat.java
@@ -0,0 +1,13 @@
+package org.scope.models;
+
+public class Cat {
+    private String message = "May";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/org/scope/models/Dog.java
@@ -0,0 +1,13 @@
+package org.scope.models;
+
+public class Dog {
+    private String message = "Woof";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
